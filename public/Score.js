f514af2942f72fc0f5a0ff49381fcc8e20e95610
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-06-14 12:59:14 +0900

    Update Score.js

--- public/Score.js
+++ public/Score.js
@@ -1,6 +1,16 @@
+import { sendEvent } from './Socket.js';
+import stages from './assets/stage.json' with { type: 'json' };
+import items from './assets/item.json' with { type: 'json' };
+
 class Score {
+  static highScore = 0;
   score = 0;
   HIGH_SCORE_KEY = 'highScore';
+  stageChange = true;
+  stageLevel = 0;
+  currentStageId = 1000;
+  targetStageId = 1001;
+  scorePerSecond = 1;
 
   constructor(ctx, scaleRatio) {
     this.ctx = ctx;
@@ -8,23 +18,62 @@ class Score {
     this.scaleRatio = scaleRatio;
   }
 
+  moveNextStage() {
+    this.stageLevel++;
+    console.log(`Stage Level : ${this.stageLevel}`);
+    sendEvent(11, { currentStage: this.currentStageId, targetStage: this.targetStageId });
+    this.currentStageId = this.targetStageId;
+    this.targetStageId++;
+  }
+
   update(deltaTime) {
-    this.score += deltaTime * 0.001;
+    this.scorePerSecond = stages.data[this.stageLevel].scorePerSecond;
+    this.score += deltaTime * 0.001 * this.scorePerSecond;
+
+    const index = stages.data.findIndex((e) => e.id === this.targetStageId);
+    const stageLength = stages.data.length;
+
+    let nextStageScore = null;
+    if (index === stageLength - 1) {
+      this.stageChange = false;
+    } else {
+      nextStageScore = stages.data[index].score;
+    }
+
+    if (Math.floor(this.score) >= nextStageScore && this.stageChange) {
+      this.moveNextStage();
+    }
+  }
+
+  // 현재 스테이지 레벨 리턴
+  getStageLevel() {
+    return this.stageLevel;
   }
 
   getItem(itemId) {
-    this.score += 0;
+    // 현재 점수에 아이템 획득 점수 추가
+    const index = items.data.findIndex((e) => e.id === itemId);
+    this.score += items.data[index].score;
+
+    // 아이템 획득 시 서버에 메시지 전송
+    sendEvent(21, {
+      itemId,
+      score: items.data[index].score,
+      stageId: this.currentStageId,
+      timestamp: Date.now(),
+    });
   }
 
   reset() {
     this.score = 0;
   }
 
-  setHighScore() {
-    const highScore = Number(localStorage.getItem(this.HIGH_SCORE_KEY));
-    if (this.score > highScore) {
-      localStorage.setItem(this.HIGH_SCORE_KEY, Math.floor(this.score));
-    }
+  static setHighScore(data) {
+    Score.highScore = Math.floor(data);
+  }
+
+  static getHighScore() {
+    return Score.highScore;
   }
 
   getScore() {
@@ -32,7 +81,7 @@ class Score {
   }
 
   draw() {
-    const highScore = Number(localStorage.getItem(this.HIGH_SCORE_KEY));
+    const drawHighScore = Score.highScore;
     const y = 20 * this.scaleRatio;
 
     const fontSize = 20 * this.scaleRatio;
@@ -43,7 +92,7 @@ class Score {
     const highScoreX = scoreX - 125 * this.scaleRatio;
 
     const scorePadded = Math.floor(this.score).toString().padStart(6, 0);
-    const highScorePadded = highScore.toString().padStart(6, 0);
+    const highScorePadded = drawHighScore.toString().padStart(6, 0);
 
     this.ctx.fillText(scorePadded, scoreX, y);
     this.ctx.fillText(`HI ${highScorePadded}`, highScoreX, y);
